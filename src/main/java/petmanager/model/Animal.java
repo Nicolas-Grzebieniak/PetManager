b10e5584609a06dbfac759bf7c649e46b9ef1053
Nicolas-Grzebieniak <nicolasjacintog@gmail.com>
2025-11-12 15:07:17 -0300

    Classe abstrata animal ainda incompleta

---
+++ src/main/java/petmanager/model/Animal.java
@@ -0,0 +1,22 @@
+package petmanager.model;
+
+public abstract class Animal {
+    private String nome;
+    private int idade;
+    private String tipo;
+    private Double peso;
+
+    public Animal(String nome, int idade, String tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
