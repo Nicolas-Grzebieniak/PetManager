1b23ba9e94780ae985b07112fc5d9274f1363601
Nicolas-Grzebieniak <nicolasjacintog@gmail.com>
2025-11-13 18:04:35 -0300

    Classe main para testar a instância das subclasses Cachorro e Gato

---
+++ src/main/java/petmanager/model/Main.java
@@ -0,0 +1,16 @@
+package petmanager.model;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Cachorro dog = new Cachorro("Thor", 3,"Doberman", true);
+        Gato cat = new Gato("Poro", 2, "Branco", false);
+
+        System.out.println(dog);
+        System.out.println("Som: " + dog.emitirSom());
+
+        System.out.println(cat);
+        System.out.println("Som: " + cat.emitirSom()
+    }
+}
